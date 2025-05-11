0d450eed686cf4ad0f5f49b6ca2b603487e50a91
Anthony Champagne <dev@anthonychampagne.fr>
2025-05-11 18:50:44 +0200

    urfave/cli v2 to v3, upgrade deps & tidy

--- src/go.mod
+++ src/go.mod
@@ -1,6 +1,6 @@
 module github.com/anthochamp/docker-plugin-vaultfs
 
-go 1.23.0
+go 1.24
 
 toolchain go1.24.3
 
@@ -10,33 +10,26 @@ require (
 	github.com/hashicorp/vault/api v1.16.0
 	github.com/hashicorp/vault/api/auth/approle v0.9.0
 	github.com/hashicorp/vault/api/auth/userpass v0.9.0
-	github.com/urfave/cli/v2 v2.27.6
 	github.com/urfave/cli/v3 v3.3.3
 	golang.org/x/sys v0.33.0
 )
 
 require (
-	github.com/cenkalti/backoff/v3 v3.2.2 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
-	github.com/go-jose/go-jose/v3 v3.0.3 // indirect
-	github.com/go-jose/go-jose/v4 v4.0.1 // indirect
+	github.com/go-jose/go-jose/v4 v4.1.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.7 // indirect
 	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
-	github.com/hashicorp/go-secure-stdlib/parseutil v0.1.8 // indirect
+	github.com/hashicorp/go-secure-stdlib/parseutil v0.2.0 // indirect
 	github.com/hashicorp/go-secure-stdlib/strutil v0.1.2 // indirect
-	github.com/hashicorp/go-sockaddr v1.0.6 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.7 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
-	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
-	golang.org/x/crypto v0.32.0 // indirect
-	golang.org/x/net v0.34.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
-	golang.org/x/time v0.5.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+	golang.org/x/time v0.11.0 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/signal"
@@ -19,7 +20,7 @@ import (
 	dockerSdkPlugin "github.com/anthochamp/docker-plugin-vaultfs/dockersdk/plugin"
 	"github.com/anthochamp/docker-plugin-vaultfs/options"
 	"github.com/anthochamp/docker-plugin-vaultfs/util"
-	cli "github.com/urfave/cli/v2"
+	cli "github.com/urfave/cli/v3"
 )
 
 var (
@@ -34,34 +35,34 @@ func main() {
 
 	defaultOptDocker := options.MakeOptDocker()
 
-	app := &cli.App{
+	app := &cli.Command{
 		Name:    constants.AppName,
 		Version: fmt.Sprintf("%s, build %s+%s (%s-%s-%s)", appVersion, commitHash, buildDate, runtime.Compiler, runtime.GOOS, runtime.GOARCH),
 		Flags: []cli.Flag{
 			&cli.BoolFlag{
 				Name:        "debug",
-				EnvVars:     []string{constants.EnvVarsPrefix + "DEBUG"},
+				Sources:     cli.EnvVars(constants.EnvVarsPrefix + "DEBUG"),
 				Value:       false,
 				Usage:       "Debug mode (WARNING: it will leak sensitive data in logs)",
 				Destination: &util.DebugMode,
 			},
 			&cli.BoolFlag{
 				Name:        "verbose",
-				EnvVars:     []string{constants.EnvVarsPrefix + "VERBOSE"},
+				Sources:     cli.EnvVars(constants.EnvVarsPrefix + "VERBOSE"),
 				Value:       false,
 				Usage:       "Verbose mode",
 				Destination: &util.Verbose,
 			},
 			&cli.BoolFlag{
 				Name:    "disable-mlock",
-				EnvVars: []string{constants.EnvVarsPrefix + "DISABLE_MLOCK"},
+				Sources: cli.EnvVars(constants.EnvVarsPrefix + "DISABLE_MLOCK"),
 				Value:   false,
 				Usage:   "Disable memory locking (NOT RECOMMENDED)",
 			},
 			&cli.StringFlag{
 				Category:    "Vault Client Options",
 				Name:        "vault-url",
-				EnvVars:     []string{constants.EnvVarsPrefix + "VAULT_URL", "VAULT_ADDR"},
+				Sources:     cli.EnvVars(constants.EnvVarsPrefix+"VAULT_URL", "VAULT_ADDR"),
 				Usage:       "URL of the Vault Server",
 				Destination: &defaultOptDocker.Secret.Vault.ClientHttp.Address,
 				Required:    true,
@@ -69,7 +70,7 @@ func main() {
 			&cli.BoolFlag{
 				Category:    "Vault Client Options",
 				Name:        "vault-disable-redirects",
-				EnvVars:     []string{constants.EnvVarsPrefix + "VAULT_DISABLE_REDIRECTS"},
+				Sources:     cli.EnvVars(constants.EnvVarsPrefix + "VAULT_DISABLE_REDIRECTS"),
 				Value:       defaultOptDocker.Secret.Vault.ClientHttp.DisableRedirects,
 				Usage:       "Disable Vault HTTP redirects",
 				Destination: &defaultOptDocker.Secret.Vault.ClientHttp.DisableRedirects,
@@ -77,7 +78,7 @@ func main() {
 			&cli.BoolFlag{
 				Category:    "Vault Client Options",
 				Name:        "vault-tls-skip-verify",
-				EnvVars:     []string{constants.EnvVarsPrefix + "VAULT_TLS_SKIP_VERIFY", "VAULT_SKIP_VERIFY"},
+				Sources:     cli.EnvVars(constants.EnvVarsPrefix+"VAULT_TLS_SKIP_VERIFY", "VAULT_SKIP_VERIFY"),
 				Value:       defaultOptDocker.Secret.Vault.ClientHttp.Tls.Insecure,
 				Usage:       "Skip verification of Vault server TLS certificate",
 				Destination: &defaultOptDocker.Secret.Vault.ClientHttp.Tls.Insecure,
@@ -86,7 +87,7 @@ func main() {
 			&cli.StringFlag{
 				Category:    "Vault Client Options",
 				Name:        "auth-method",
-				EnvVars:     []string{constants.EnvVarsPrefix + "AUTH_METHOD"},
+				Sources:     cli.EnvVars(constants.EnvVarsPrefix + "AUTH_METHOD"),
 				Value:       defaultOptDocker.Secret.Vault.VaultAuth.Method,
 				Usage:       "Default Auth method (AppRole, Cert, Token, Userpass)",
 				Destination: &defaultOptDocker.Secret.Vault.VaultAuth.Method,
@@ -94,7 +95,7 @@ func main() {
 			&cli.StringFlag{
 				Category:    "Vault Client Options",
 				Name:        "auth-mount",
-				EnvVars:     []string{constants.EnvVarsPrefix + "AUTH_MOUNT"},
+				Sources:     cli.EnvVars(constants.EnvVarsPrefix + "AUTH_MOUNT"),
 				Value:       "",
 				DefaultText: defaultOptDocker.Secret.Vault.VaultAuth.EffectiveMountPath(),
 				Usage:       "Default Auth engine mount path",
@@ -103,7 +104,7 @@ func main() {
 			&cli.StringFlag{
 				Category:    "Vault Secrets",
 				Name:        "engine-type",
-				EnvVars:     []string{constants.EnvVarsPrefix + "ENGINE_TYPE"},
+				Sources:     cli.EnvVars(constants.EnvVarsPrefix + "ENGINE_TYPE"),
 				Value:       defaultOptDocker.Secret.Vault.VaultEngine.Type,
 				Usage:       "Default Vault Secrets engine type (KV, DB or PKI)",
 				Destination: &defaultOptDocker.Secret.Vault.VaultEngine.Type,
@@ -111,7 +112,7 @@ func main() {
 			&cli.StringFlag{
 				Category:    "Vault Secrets",
 				Name:        "engine-mount",
-				EnvVars:     []string{constants.EnvVarsPrefix + "ENGINE_MOUNT"},
+				Sources:     cli.EnvVars(constants.EnvVarsPrefix + "ENGINE_MOUNT"),
 				Value:       "",
 				DefaultText: defaultOptDocker.Secret.Vault.VaultEngine.EffectiveMountPath(),
 				Usage:       "Default Vault Secrets engine mount path",
@@ -119,7 +120,7 @@ func main() {
 			&cli.IntFlag{
 				Category:    "Vault Secrets",
 				Name:        "kv-engine-version",
-				EnvVars:     []string{constants.EnvVarsPrefix + "KV_ENGINE_VERSION"},
+				Sources:     cli.EnvVars(constants.EnvVarsPrefix + "KV_ENGINE_VERSION"),
 				Value:       defaultOptDocker.Secret.Vault.VaultEngine.KvVersion,
 				Usage:       "Default Vault Secrets K/V engine version (1 or 2)",
 				Destination: &defaultOptDocker.Secret.Vault.VaultEngine.KvVersion,
@@ -128,14 +129,14 @@ func main() {
 			&cli.StringFlag{
 				Category: "Docker Plugin",
 				Name:     "plugin-tcp-bind-addr",
-				EnvVars:  []string{constants.EnvVarsPrefix + "PLUGIN_TCP_BIND_ADDR"},
+				Sources:  cli.EnvVars(constants.EnvVarsPrefix + "PLUGIN_TCP_BIND_ADDR"),
 				Value:    "0.0.0.0",
 				Usage:    "Docker Plugin TCP bind address (IPv4 or IPv6)",
 			},
 			&cli.UintFlag{
 				Category:    "Docker Plugin",
 				Name:        "plugin-tcp-bind-port",
-				EnvVars:     []string{constants.EnvVarsPrefix + "PLUGIN_TCP_BIND_PORT"},
+				Sources:     cli.EnvVars(constants.EnvVarsPrefix + "PLUGIN_TCP_BIND_PORT"),
 				Value:       0,
 				DefaultText: "<undefined>",
 				Usage:       "Docker Plugin TCP bind port",
@@ -143,28 +144,28 @@ func main() {
 			&cli.StringFlag{
 				Category: "Docker Plugin",
 				Name:     "plugin-socket-path",
-				EnvVars:  []string{constants.EnvVarsPrefix + "PLUGIN_SOCKET_PATH"},
+				Sources:  cli.EnvVars(constants.EnvVarsPrefix + "PLUGIN_SOCKET_PATH"),
 				Value:    "/run/docker/plugins/" + constants.DockerPluginId + ".sock",
 				Usage:    "Docker Plugin Unix socket path",
 			},
 			&cli.StringFlag{
 				Category: "Docker Plugin",
 				Name:     "plugin-socket-user",
-				EnvVars:  []string{constants.EnvVarsPrefix + "PLUGIN_SOCKET_USER"},
+				Sources:  cli.EnvVars(constants.EnvVarsPrefix + "PLUGIN_SOCKET_USER"),
 				Value:    currentUser.Username,
 				Usage:    "Docker Plugin Unix socket user name or ID",
 			},
 			&cli.StringFlag{
 				Category: "Docker Plugin",
 				Name:     "plugin-socket-group",
-				EnvVars:  []string{constants.EnvVarsPrefix + "PLUGIN_SOCKET_GROUP"},
+				Sources:  cli.EnvVars(constants.EnvVarsPrefix + "PLUGIN_SOCKET_GROUP"),
 				Value:    currentGroup.Name,
 				Usage:    "Docker Plugin Unix socket group name or ID",
 			},
 			&cli.UintFlag{
 				Category:    "Docker Plugin",
 				Name:        "plugin-socket-mode",
-				EnvVars:     []string{constants.EnvVarsPrefix + "PLUGIN_SOCKET_MODE"},
+				Sources:     cli.EnvVars(constants.EnvVarsPrefix + "PLUGIN_SOCKET_MODE"),
 				Value:       0600,
 				DefaultText: "0600",
 				Usage:       "Docker Plugin Unix socket access modes",
@@ -172,112 +173,112 @@ func main() {
 			&cli.BoolFlag{
 				Category: "Docker Volume Driver",
 				Name:     "disable-volume-driver",
-				EnvVars:  []string{constants.EnvVarsPrefix + "DISABLE_VOLUME_DRIVER"},
+				Sources:  cli.EnvVars(constants.EnvVarsPrefix + "DISABLE_VOLUME_DRIVER"),
 				Value:    false,
 				Usage:    "Disable Volume Driver",
 			},
 			&cli.BoolFlag{
 				Category: "Docker Volume Driver",
 				Name:     "volume-driver-global-scope",
-				EnvVars:  []string{constants.EnvVarsPrefix + "VOLUME_DRIVER_GLOBAL_SCOPE"},
+				Sources:  cli.EnvVars(constants.EnvVarsPrefix + "VOLUME_DRIVER_GLOBAL_SCOPE"),
 				Value:    false,
 				Usage:    "Use global scope instead of local scope",
 			},
 			&cli.StringFlag{
 				Category: "Docker Volume Driver",
 				Name:     "volume-driver-state-file",
-				EnvVars:  []string{constants.EnvVarsPrefix + "VOLUME_DRIVER_STATE_FILE"},
+				Sources:  cli.EnvVars(constants.EnvVarsPrefix + "VOLUME_DRIVER_STATE_FILE"),
 				Value:    path.Join("/var/local", constants.AppName, "state.json"),
 				Usage:    "Volume Driver state file",
 			},
 			&cli.StringFlag{
 				Category: "Docker Volume Driver",
 				Name:     "volume-driver-mount-dir",
-				EnvVars:  []string{constants.EnvVarsPrefix + "VOLUME_DRIVER_MOUNT_DIR"},
+				Sources:  cli.EnvVars(constants.EnvVarsPrefix + "VOLUME_DRIVER_MOUNT_DIR"),
 				Value:    path.Join(dockerSdkPlugin.DefaultDockerRootDirectory, constants.DockerPluginId),
 				Usage:    "Volume Driver FS mount directory path",
 			},
 			&cli.StringFlag{
 				Category: "Docker Volume Driver",
 				Name:     "volume-driver-mount-user",
-				EnvVars:  []string{constants.EnvVarsPrefix + "VOLUME_DRIVER_MOUNT_USER"},
+				Sources:  cli.EnvVars(constants.EnvVarsPrefix + "VOLUME_DRIVER_MOUNT_USER"),
 				Value:    currentUser.Username,
 				Usage:    "Volume Driver FS mount user name or ID",
 			},
 			&cli.StringFlag{
 				Category: "Docker Volume Driver",
 				Name:     "volume-driver-mount-group",
-				EnvVars:  []string{constants.EnvVarsPrefix + "VOLUME_DRIVER_MOUNT_GROUP"},
+				Sources:  cli.EnvVars(constants.EnvVarsPrefix + "VOLUME_DRIVER_MOUNT_GROUP"),
 				Value:    currentGroup.Name,
 				Usage:    "Volume Driver FS mount group name or ID",
 			},
 			&cli.BoolFlag{
 				Category: "Docker Secret Provider",
 				Name:     "disable-secret-provider",
-				EnvVars:  []string{constants.EnvVarsPrefix + "DISABLE_SECRET_PROVIDER"},
+				Sources:  cli.EnvVars(constants.EnvVarsPrefix + "DISABLE_SECRET_PROVIDER"),
 				Value:    false,
 				Usage:    "Disable Secret Provider",
 			},
 		},
-		Action: func(ctx *cli.Context) error {
-			if ctx.IsSet("auth-mount") {
-				v := ctx.String("auth-mount")
+		Action: func(ctx context.Context, c *cli.Command) error {
+			if c.IsSet("auth-mount") {
+				v := c.String("auth-mount")
 				defaultOptDocker.Secret.Vault.VaultAuth.MountPath = &v
 			}
 
-			if ctx.IsSet("engine-mount") {
-				v := ctx.String("engine-mount")
+			if c.IsSet("engine-mount") {
+				v := c.String("engine-mount")
 				defaultOptDocker.Secret.Vault.VaultEngine.MountPath = &v
 			}
 
-			return start(ctx, defaultOptDocker)
+			return start(ctx, c, defaultOptDocker)
 		},
 	}
 
-	if err := app.Run(os.Args); err != nil {
+	if err := app.Run(context.Background(), os.Args); err != nil {
 		util.Fatalf("%v\n", err)
 	}
 }
 
-func start(ctx *cli.Context, defaultOptDocker options.OptDocker) error {
+func start(ctx context.Context, c *cli.Command, defaultOptDocker options.OptDocker) error {
 	var arg string
 
-	util.Printf("Plugin starting. Version: %s\n", ctx.App.Version)
+	util.Printf("Plugin starting. Version: %s\n", c.Version)
 
-	arg = ctx.String("plugin-socket-user")
+	arg = c.String("plugin-socket-user")
 	unixSocketUId, err := util.UserIdFromUser(arg)
 	if err != nil {
 		return fmt.Errorf("unable to find user %s: %w", arg, err)
 	}
 
-	arg = ctx.String("plugin-socket-group")
+	arg = c.String("plugin-socket-group")
 	unixSocketGId, err := util.UserGroupIdFromUserGroup(arg)
 	if err != nil {
 		return fmt.Errorf("unable to find group %s: %w", arg, err)
 	}
 
-	arg = ctx.String("volume-driver-mount-user")
+	arg = c.String("volume-driver-mount-user")
 	mountDirUId, err := util.UserIdFromUser(arg)
 	if err != nil {
 		return fmt.Errorf("unable to find user %s: %w", arg, err)
 	}
 
-	arg = ctx.String("volume-driver-mount-group")
+	arg = c.String("volume-driver-mount-group")
 	mountDirGId, err := util.UserGroupIdFromUserGroup(arg)
 	if err != nil {
 		return fmt.Errorf("unable to find group %s: %w", arg, err)
 	}
 
-	tcpBindAddr := ctx.String("plugin-tcp-bind-addr")
+	tcpBindAddr := c.String("plugin-tcp-bind-addr")
 
 	var tcpBindPort *uint16
-	ptbp := ctx.Uint("plugin-tcp-bind-port")
+	ptbp := c.Uint("plugin-tcp-bind-port")
 	if ptbp != 0 {
 		tbp16 := uint16(ptbp)
 		tcpBindPort = &tbp16
 	}
 
-	unixSocketPath := ctx.String("plugin-socket-path")
+	unixSocketPath := c.String("plugin-socket-path")
 
 	dockerPlugin, err := docker.NewPlugin(docker.PluginConfig{
 		TcpBindAddr:    &tcpBindAddr,
@@ -286,19 +287,19 @@ func start(ctx *cli.Context, defaultOptDocker options.OptDocker) error {
 		UnixSocketPath: &unixSocketPath,
 		UnixSocketUId:  unixSocketUId,
 		UnixSocketGId:  unixSocketGId,
-		UnixSocketMode: uint32(ctx.Uint("plugin-socket-mode")),
+		UnixSocketMode: uint32(c.Uint("plugin-socket-mode")),
 
-		VolumeDriverDisabled:      ctx.Bool("disable-volume-driver"),
-		VolumeDriverGlobalScope:   ctx.Bool("volume-driver-global-scope"),
-		VolumeDriverStateFilePath: ctx.String("volume-driver-state-file"),
+		VolumeDriverDisabled:      c.Bool("disable-volume-driver"),
+		VolumeDriverGlobalScope:   c.Bool("volume-driver-global-scope"),
+		VolumeDriverStateFilePath: c.String("volume-driver-state-file"),
 		VolumeDriverFsConfig: docker.FsConfig{
 			MountFuseName: constants.AppName,
-			MountDir:      ctx.String("volume-driver-mount-dir"),
+			MountDir:      c.String("volume-driver-mount-dir"),
 			MountDirUId:   mountDirUId,
 			MountDirGId:   mountDirGId,
 		},
 
-		SecretProviderDisabled: ctx.Bool("disable-secret-provider"),
+		SecretProviderDisabled: c.Bool("disable-secret-provider"),
 
 		DefaultOptDocker: defaultOptDocker,
 	})
@@ -306,7 +307,7 @@ func start(ctx *cli.Context, defaultOptDocker options.OptDocker) error {
 		return fmt.Errorf("create plugin: %w", err)
 	}
 
-	if !ctx.Bool("disable-mlock") {
+	if !c.Bool("disable-mlock") {
 		if err := util.LockMemory(); err != nil {
 			return fmt.Errorf("lock memory: %w", err)
 		}
